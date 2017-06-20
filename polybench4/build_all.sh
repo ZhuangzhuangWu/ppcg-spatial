29ed5575c08edb7b78f4c369cff22d84656cc9ca
Oleksandr Zinenko <ftynse@gmail.com>
2017-06-20 05:38:13 -0400

    [pb] Remove "default" configuration from build_all

--- polybench4/build_all.sh
+++ polybench4/build_all.sh
@@ -2,6 +2,5 @@
 ./build.sh \
     orig "" \
     pluto "" \
-    default "--target=c --openmp --tile" \
     spatial "--target=c --openmp --tile --isl-schedule-single-outer-coincidence --no-isl-schedule-maximize-coincidence --isl-schedule-outer-typed-fusion --isl-schedule-spatial-fusion  --isl-schedule-max-coefficient=4 --isl-schedule-max-constant-term=10 --no-isl-schedule-parametric -DPOLYBENCH_USE_C99_PROTO  --isl-schedule-avoid-inner-coincidence  --wavefront=single --isl-schedule-outer-coincidence --no-isl-schedule-force-outer-coincidence" \
     posttile "--target=c --openmp --tile --isl-schedule-single-outer-coincidence --no-isl-schedule-maximize-coincidence --isl-schedule-outer-typed-fusion --isl-schedule-spatial-fusion --isl-schedule-max-coefficient=4 --isl-schedule-max-constant-term=10 --no-isl-schedule-parametric -DPOLYBENCH_USE_C99_PROTO --isl-schedule-avoid-inner-coincidence --wavefront=single --isl-schedule-outer-coincidence --no-isl-schedule-force-outer-coincidence --posttile-reorder=pluto"
